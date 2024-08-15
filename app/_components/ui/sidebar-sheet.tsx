522ab1366e69d230a0e2747ca0080b177df0deb3
SrIury <iuryoliveirabonadiman@gmail.com>
2024-08-15 01:13:06 -0300

    chore: add next auth

--- app/_components/ui/sidebar-sheet.tsx
+++ app/_components/ui/sidebar-sheet.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react";
 import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
 import { quickSearchOptions } from "@/app/_constants/search";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./dialog";
 
 const SidebarSheet = () => {
     return (
@@ -15,15 +16,35 @@ const SidebarSheet = () => {
                 </SheetTitle>
             </SheetHeader>
 
-            <div className="flex items-center border-b border-solid py-5 gap-3">
-                <Avatar>
+            <div className="flex items-center justify-between border-b border-solid py-5 gap-3">
+                <h2 className="font-bold">Olá, faça seu login</h2>
+                <Dialog>
+                    <DialogTrigger>
+                        <Button size="icon">
+                            <LogInIcon />
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="w-[90%]">
+                        <DialogHeader>
+                            <DialogTitle>Faça login na plataforma</DialogTitle>
+                            <DialogDescription>
+                                Conecte-se usando sua conta do Google.
+                            </DialogDescription>
+                        </DialogHeader>
+
+                        <Button variant="outline" className="gap-1 font-bold" >
+                            <Image src="/Google.svg" width={18} height={18} />Google
+                        </Button>
+                    </DialogContent>
+                </Dialog>
+                {/* <Avatar>
                     <AvatarImage src="https://images.unsplash.com/photo-1623582854588-d60de57fa33f?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
                 </Avatar>
 
                 <div>
                     <p className="font-bold">Iury Bonadiman</p>
                     <p className="text-xs">iury.bonadiman@gmail.com</p>
-                </div>
+                </div> */}
             </div>
 
             {/* Botões menu Lateral */}
